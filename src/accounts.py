02a4d6ea0d397d0f398546db33274f228b2887a8
Martin <martindoornhein@gmail.com>
2024-07-03 18:39:49 +0200

    small code upgrade to def __init__

--- src/accounts.py
+++ src/accounts.py
@@ -2,18 +2,22 @@ from abc import ABC, abstractmethod
 from datetime import datetime
 
 class AccountBase(ABC):
-    def __init__(self, owner: str, account_number: int, balance: int):
-        self.owner = owner
-        self.account_number = account_number
-        self.balance = balance
+    def __init__(self, account_name: str, bank_name, balance: float = 0) -> None:
+        
+        """
+        Initiliazation method for AccountBase Subclasses
+        """
+        
+        self._account_name = account_name
+        self._bank_name = bank_name
         self._created = datetime.now()
         self._updates = None
+        self._balance = balance
     
     @abstractmethod
     def deposit(self, amount: int) -> None:
-        self.balance += amount
-        print(f"succesful withdrawed: {amount}")
-        print(f"new balance = {self.balance}")
+        pass
+
 
     def withdraw(self, amount: int) -> None:
         if self.balance >= amount:  
@@ -25,8 +29,8 @@ class AccountBase(ABC):
             print("you ran out of money friend")
 
 class Savings_Account(AccountBase):
-    def __init__(self, name, account_number, balance, interest_rate):
-        super().__init__(name, account_number, balance)
+    def __init__(self, name: str, balance: float, interest_rate: float = 1.05):
+        super().__init__(name, balance)
         self.interest_rate = interest_rate
 
     def deposit(self, amount: int):
@@ -43,5 +47,6 @@ class Savings_Account(AccountBase):
         print(self.balance * (self.interest_rate  ** n_years))
 
     
-  
+class StudentAccount(AccountBase):
+    pass
 
