87c17609dbc05472b83afb9813b5771b23882061
Martin <martindoornhein@gmail.com>
2024-07-03 18:40:19 +0200

    add bank class to main

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import src.accounts as acc
+import src.bank as bank
 
-if __name__ == "__main__":      
-    s_acc1 = acc.Savings_Account("martin", 123, 1000, 1.05)
-    s_acc1.compound_interest_calculator(7)
-    s_acc1.add_interest()
+
+b = bank.Bank("md_bank")
+
+acc1 = b.create_savings_account("John", 4000)
+acc2 = b.create_savings_account("berth", 2000)
+print(acc1._created)
+print(acc1.balance)
+print(acc1.withdraw(200))
+print(acc1.balance)
+
+print(b.list_accounts())
+
+# if __name__ == "__main__":      
+#     s_acc1 = acc.Savings_Account("martin", 123, 1000, 1.05)
+#     s_acc1.compound_interest_calculator(7)
+#     s_acc1.add_interest()
 
 
     
