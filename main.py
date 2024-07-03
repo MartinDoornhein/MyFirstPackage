e4fd11aa8fe148916d82c6cf319d2c3976ac4d53
Martin Doornhein <M.Doornhein@nl.aswatson.com>
2024-07-03 19:44:07 +0200

    fixed issue with mixing up class properties

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ import src.bank as bank
 
 b = bank.Bank("md_bank")
 
-acc1 = b.create_savings_account("John", 4000)
-acc2 = b.create_savings_account("berth", 2000)
-print(acc1._created)
-print(acc1.balance)
-print(acc1.withdraw(200))
+acc1 = b.create_savings_account(account_name="John", initial_deposit=4000)
+acc2 = b.create_savings_account(account_name="berth", initial_deposit=2000)
+# print(acc1._created)
+# print(acc1.balance)
+# print(acc1.withdraw(200))
+# print(acc1.balance)
+
+for i in b.list_accounts():
+    print(i.info())
 print(acc1.balance)
 
-print(b.list_accounts())
 
-# if __name__ == "__main__":      
+# if __name__ == "__main__":
 #     s_acc1 = acc.Savings_Account("martin", 123, 1000, 1.05)
 #     s_acc1.compound_interest_calculator(7)
 #     s_acc1.add_interest()
-
-
-    
--- src/accounts.py
+++ src/accounts.py
@@ -1,26 +1,25 @@
 from abc import ABC, abstractmethod
 from datetime import datetime
 
+
 class AccountBase(ABC):
-    def __init__(self, account_name: str, bank_name, balance: float = 0) -> None:
-        
+    def __init__(self, account_name: str, bank_name, balance: float) -> None:
         """
         Initiliazation method for AccountBase Subclasses
         """
-        
+
         self._account_name = account_name
         self._bank_name = bank_name
         self._created = datetime.now()
         self._updates = None
-        self._balance = balance
-    
+        self.balance = balance
+
     @abstractmethod
     def deposit(self, amount: int) -> None:
         pass
 
-
     def withdraw(self, amount: int) -> None:
-        if self.balance >= amount:  
+        if self.balance >= amount:
             self.balance -= amount
             self._created = datetime.now()
             print(f"succesful withdrawed: {amount}")
@@ -28,9 +27,22 @@ class AccountBase(ABC):
         else:
             print("you ran out of money friend")
 
+    def info(self):
+        print(f"bank name: {self._bank_name}")
+        print(f"account_name: {self._account_name}")
+        print(f"created account on: {self._created}")
+        print(f"balance: {self.balance}")
+
+
 class Savings_Account(AccountBase):
-    def __init__(self, name: str, balance: float, interest_rate: float = 1.05):
-        super().__init__(name, balance)
+    def __init__(
+        self,
+        account_name: str,
+        bank_name: str,
+        balance: float = 0,
+        interest_rate: float = 1.05,
+    ):
+        super().__init__(account_name, bank_name, balance)
         self.interest_rate = interest_rate
 
     def deposit(self, amount: int):
@@ -42,11 +54,10 @@ class Savings_Account(AccountBase):
         self.balance *= self.interest_rate
         print("your annual interest is added")
         print(f"your new balance = {self.balance}")
-    
-    def compound_interest_calculator(self, n_years):
-        print(self.balance * (self.interest_rate  ** n_years))
 
-    
+    def compound_interest_calculator(self, n_years):
+        print(self.balance * (self.interest_rate**n_years))
+
+
 class StudentAccount(AccountBase):
     pass
-
